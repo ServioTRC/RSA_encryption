886ad4b6bb241d583f6e699abe124b5ab1fbec0e
Servss <A01371719@itesm.mx>
2017-11-08 19:08:41 -0600

    Encripción y desencripción de mensajes usando RSA

--- rsa.py
+++ rsa.py
@@ -1,4 +1,4 @@
-import random 
+import random
 
 def is_prime(number):
     if number % 2 == 0 and number > 2:
@@ -22,21 +22,30 @@ def extended_gcd(a, b):
     old_s = 1
     old_t = 0
     old_r = a
-    while r != 0:
+    while r > 0:
         quotient = old_r // r
         old_r, r = r, (old_r - quotient*r)
         old_s, s = s, (old_s - quotient*s)
         old_t, t = t, (old_t - quotient*t)
-    if old_r == 1:
-        return s+old_s
-    else:
-        return old_r        
+    return s+old_s       
 
 def enciphering_exponent(n, rsa_modulus):
     while True:
         e = random.randint(2, rsa_modulus-1)
         if gcd(e, rsa_modulus) == 1:
             return e
+
+def encrypter(message, e, n):
+    message_encrypt = []
+    for letter in message:
+        message_encrypt.append(ord(letter) ** e % n)
+    return message_encrypt
+
+def decrypter(message, d, n):
+    message_decrypt = ""
+    for val in message:
+        message_decrypt += chr(int(val ** d % n))
+    return message_decrypt
 
 def main():
     while True:
@@ -47,8 +56,8 @@ def main():
             else:
                 print("It isn't a prime number, please retry.")
         while True:      
-            prime2 = int(input("Enter the first prime number: "))
-            if is_prime(prime1):
+            prime2 = int(input("Enter the second prime number: "))
+            if is_prime(prime2):
                 break
             else:
                 print("It isn't a prime number, please retry.")
@@ -56,11 +65,22 @@ def main():
             break
         else:
             print("Prime numbers must not be equal")
+    
     n = prime1*prime2
     rsa_modulus = (prime1-1)*(prime2-1)
-    print("n:", n, "rsa_modulus:", rsa_modulus)
     e = enciphering_exponent(n, rsa_modulus)
     d = extended_gcd(e, rsa_modulus)
-    print("e:", e, "d:", d)
+    print("RSA modulus:", rsa_modulus)
+    print("Public key values:\n\tn =", n, "\te =", e)
+    print("Private key values:\n\tn =", n, "\td =", d)
+    message = input("Insert the message in 'message' to encrypt : ")
+    message_encrypt = encrypter(message, e, n)
+    print("Encrypted message: ", end="")
+    for val in message_encrypt:
+        print(val, end="")
+    print()
+    message = decrypter(message_encrypt, d, n)
+    print("Decrypted message:", message)
 
-main()
+if __name__ == '__main__':
+    main()
